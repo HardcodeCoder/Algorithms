190855ad23d1b812dffd363c9715f2a1bc2a72ce
HardcodeCoder <hardcodecoder@gmail.com>
2020-04-18 15:30:32 +0530

    minWeight function must loop over all vertices

--- Graphs/dijkstra_shortest_path.cpp
+++ Graphs/dijkstra_shortest_path.cpp
@@ -2,7 +2,7 @@
  * @Author: hardcodecoder
  * @Date:   02:30:40 Sunday 05 April 2020
  * @Last modified by:   hardcodecoder
- * @Last modified time: 05:01:53 Sunday 05 April 2020
+ * @Last modified time: 03:22:46 Saturday 18 April 2020
  */
 
 
@@ -11,15 +11,18 @@
 
 #define INF 9999
 
-int *previousVertex; //array to store preceding vertex
-int *distance;       //array to store distance from source vertex(shortest distance) for each vertex
+int *previousVertex; // array to store preceding vertex
+int *distance;       // array to store distance from source vertex(shortest distance) for each vertex
+int numVertices;     // stores number of vertices in the graph
+bool *visited;       // array to mark visited vertices
 
-//Helper method to find the minimum weighted
-//vertex from the current vertex
-int minWeight(bool *visited){
+
+// Helper method to find the minimum weighted
+// vertex from the current vertex
+int minWeight(){
   int min = INF;
   int minIndex = -1;
-  for (int v = 0; v < 9; v++){
+  for (int v = 0; v < numVertices; v++){
     if (!visited[v] && distance[v] <= min)
       min = distance[v], minIndex = v;
   }
@@ -27,8 +30,8 @@ int minWeight(bool *visited){
 }
 
 
-//Recursive method to print the shortest path
-//between source and destination vertex
+// Recursive method to print the shortest path
+// between source and destination vertex
 int printPath(int src, int dest) {
   if (src == dest) return src;
   printf("%d --> ", printPath(src, previousVertex[dest]));
@@ -36,86 +39,86 @@ int printPath(int src, int dest) {
 }
 
 
-//Helper method to handle print queries
-void print(int v, int src, int dest){
+// Helper method to handle print queries
+void print(int src, int dest){
   printf("\nMinimum weight between %d -> %d = %d\n", src, dest, distance[dest]);
   printf("\nPath : ");
   printf("%d", printPath(src, dest));
 
-  //Uncomment below code to print all distances from source
+  // Uncomment below code to print all distances from source
   /*printf("\n\nVertex\tDistance from source\n");
-  for (int i = 0; i < v; i++){
+  for (int i = 0; i < numVertices; i++){
     printf("%d\t\t%d\n", i, distance[i]);
   }*/
 }
 
 
-//Implementation of dijkstra's shortest path algorithm
-void dijkstraShortestPath (int vertexCount, int **graph, int src, int dest) {
-  bool *visited; //array to mark visited vertices
+// Implementation of dijkstra's shortest path algorithm
+void dijkstraShortestPath (int **graph, int src, int dest) {
+  // allocate memory for arrays
+  previousVertex = (int*)malloc(sizeof(int) * numVertices);
+  distance = (int*)malloc(sizeof(int) * numVertices);
+  visited = (bool*)malloc(sizeof(bool) * numVertices);
 
-  //allocate memory for arrays
-  previousVertex = (int*)malloc(sizeof(int)*vertexCount);
-  distance = (int*)malloc(sizeof(int)*vertexCount);
-  visited = (bool*)malloc(sizeof(bool)*vertexCount);
-
-  for (int i = 0; i < vertexCount; i++){
+  for (int i = 0; i < numVertices; i++){
     distance[i] = INF;
     visited[i] = false;
   }
 
-  //set source vertex distance to zero
+  // set source vertex distance to zero
   distance[src] = 0;
 
-  for (int i = 0; i < vertexCount-1; i++){
+  for (int i = 0; i < numVertices-1; i++){
 
-    //returns min weighted vertex from list of
-    //non visited vertex adjacent to current vertex
-    int min = minWeight(visited);
+    // returns min weighted vertex from list of
+    // non visited vertex adjacent to current vertex
+    int min = minWeight();
 
-    //mark choosen vertex as visited
+    // mark choosen vertex as visited
     visited[min] = true;
 
-    for (int v = 0; v < vertexCount; v++){
-      //select new distance throught min if vertex is:
-      //not visited, is adjacent to current vertex,
-      //distance throught min is less than old distance
+    for (int v = 0; v < numVertices; v++){
+
+      // select new distance throught min if vertex is:
+      // not visited, is adjacent to current vertex,
+      // distance throught min is less than old distance
       if (!visited[v] && graph[min][v] && distance[min] != INF && distance[min] + graph[min][v] < distance[v]){
         distance[v] = distance[min] + graph[min][v];
-        previousVertex[v] = min;   //store the preceding vertex for vth vertex
+        previousVertex[v] = min;   // store the preceding vertex for vth vertex
       }
     }
   }
-  print(vertexCount, src, dest);
+  print(src, dest);
 }
 
 
-//driver method
+// Driver method
 int main() {
   int **graph;
-  int v, src, dest;
+  int src, dest;
 
   printf("Enter the number of vertex : ");
-  scanf("%d", &v);
+  scanf("%d", &numVertices);
   printf("\nEnter the adjacency matrix of weights:\n");
 
-  //pointer to pointer for storing 2d array
-  graph = (int**)malloc(sizeof(int*)*v);
+  // pointer to pointer for storing 2d array
+  graph = (int**)malloc(sizeof(int*) * numVertices);
 
-  for (int i = 0; i < v; i++){
-    //allocate memory from each row
-    graph[i] = (int*)malloc(sizeof(int)*v);
+  for (int i = 0; i < numVertices; i++){
+    // allocate memory from each row
+    graph[i] = (int*)malloc(sizeof(int) * numVertices);
 
-    //fill each coloum of ith row
-    for (int j = 0; j < v; j++)
-      scanf("%d", &graph[i][j]);//or use (*(graph+i)+j)
+    // fill each coloum of ith row
+    for (int j = 0; j < numVertices; j++)
+      scanf("%d", &graph[i][j]);  // or use (*(graph+i)+j)
   }
 
   printf("\nEnter the source and destination vertex : ");
   scanf("%d%d", &src, &dest);
 
-  dijkstraShortestPath(v, graph, src, dest);
+  dijkstraShortestPath(graph, src, dest);
   /*Test input
+  9
   0 4 0 0 0 0 0 8 0
   4 0 8 0 0 0 0 11 0
   0 8 0 7 0 4 0 0 2
